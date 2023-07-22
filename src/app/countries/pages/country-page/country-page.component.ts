886e3ca8b687a0f49883719209245b786eafc645
Manfred Ariel Martinez Bastos <mmartinez@palcort.com>
2023-07-22 12:28:59 -0500

    feat:  Mostrar información del país

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -12,6 +13,7 @@ import { switchMap } from 'rxjs';
 
 export class CountryPageComponent implements OnInit {
 
+  public country?: Country;
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
@@ -24,11 +26,8 @@ export class CountryPageComponent implements OnInit {
         switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id)),
       )
       .subscribe(country => {
-        if (!country) {
-          return this.router.navigateByUrl('');
-        }
-        console.log("Tenemos un pais")
-        return;
+        if (!country) return this.router.navigateByUrl('');
+        return this.country = country;
       });
   }
 }
