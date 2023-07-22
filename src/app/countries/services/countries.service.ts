41111d3cf23e318f1e233bd18b46e93664e8f4e7
Manfred Ariel Martinez Bastos <mmartinez@palcort.com>
2023-07-22 00:23:45 -0500

    feat: Pais y Region

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -17,4 +17,18 @@ export class CountriesService {
             catchError(() => of([]))
         );
     }
+
+    searchCountry(term: string): Observable<Country[]> {
+        const url = `${this.apiUrl}/name/${term}`;
+        return this.http.get<Country[]>(url).pipe(
+            catchError(() => of([]))
+        );
+    }
+
+    searchRegion(term: string): Observable<Country[]> {
+        const url = `${this.apiUrl}/region/${term}`;
+        return this.http.get<Country[]>(url).pipe(
+            catchError(() => of([]))
+        );
+    }
 }
