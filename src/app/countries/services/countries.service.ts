da4e5b82df4be881f27ba585c6e4fcb4dcbe744d
Manfred Ariel Martinez Bastos <mmartinez@palcort.com>
2023-08-05 22:14:39 -0500

    LocalStorage del Store

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -19,7 +19,17 @@ export class CountriesService {
 
 
     constructor(private http: HttpClient) {
-        console.log('CountriesService init');
+        this.loadFromLocalStorage();
+    }
+
+    private saveToLocalStorage() {
+        localStorage.setItem("cacheStorage", JSON.stringify(this.cacheStore));
+    }
+
+    private loadFromLocalStorage() {
+        if (!localStorage.getItem("cacheStorage")) return;
+        this.cacheStore = JSON.parse(localStorage.getItem("cacheStorage")!);
+
     }
 
     private getCountriesResquest(url: string): Observable<Country[]> {
@@ -42,7 +52,8 @@ export class CountriesService {
         const url = `${this.apiUrl}/capital/${term}`;
         return this.getCountriesResquest(url)
             .pipe(
-                tap(countries => this.cacheStore.byCapital = { term, countries })
+                tap(countries => this.cacheStore.byCapital = { term, countries }),
+                tap(() => this.saveToLocalStorage() )
             );
     }
 
@@ -50,7 +61,9 @@ export class CountriesService {
         const url = `${this.apiUrl}/name/${term}`;
         return this.getCountriesResquest(url)
             .pipe(
-                tap(countries => this.cacheStore.byCountries = { term, countries })
+                tap(countries => this.cacheStore.byCountries = { term, countries }),
+                tap(() => this.saveToLocalStorage() )
+
             );;
 
     }
@@ -59,7 +72,9 @@ export class CountriesService {
         const url = `${this.apiUrl}/region/${region}`;
         return this.getCountriesResquest(url)
             .pipe(
-                tap(countries => this.cacheStore.byRegion = { region, countries })
+                tap(countries => this.cacheStore.byRegion = { region, countries }),
+                tap(() => this.saveToLocalStorage() )
+
             );;
     }
 }
